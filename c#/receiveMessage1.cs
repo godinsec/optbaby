00c111a362fbe4030dca15cda1c36dd2c82212d4
Lily <z_chenli@163.com>
2014-11-25 17:09:54 +0800

    c#

---
+++ c#/receiveMessage1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using ServiceStack.Redis;
+using ServiceStack.Redis.Generic;
+using ServiceStack.Text;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication1
+{
+
+    public class Program
+    {
+        private static RedisClient redisCli = null;
+        private static string list = "job";
+
+        public static void connectRedis()
+        {
+            if (redisCli == null)
+            {
+                redisCli = new RedisClient("127.0.0.1", 6379);
+            }
+        }
+
+    
+
+        static void Main(string[] args)
+        {
+            //testConnect();
+            connectRedis();
+
+            Console.WriteLine("receiveMessage 1 pop ....");
+
+            while (true)
+            {
+                byte[][] value = redisCli.BLPop(list, 200);
+                string alist = Encoding.Default.GetString(value[0]);
+                string avalue = Encoding.Default.GetString(value[1]);
+                //Console.WriteLine(alist + " : " + avalue);
+                JsonReader reader = new JsonTextReader(new StringReader(avalue));
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.Value);
+                }
+            }
+        }
+    }
+
+
+}
---
+++ c#/sendMessage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using ServiceStack.Redis;
+using ServiceStack.Redis.Generic;
+using ServiceStack.Text;
+
+namespace ConsoleApplication1
+{
+    
+
+    public class Program
+    {
+        private static RedisClient redisCli = null;
+        private static string list = "job";
+
+        public static void connectRedis()
+        {
+            if (redisCli == null)
+            {
+                redisCli = new RedisClient("127.0.0.1", 6379);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            //testConnect();
+            connectRedis();
+            //string[] strs = { "a", "b", "c", "d", "e", "f", "g" };
+            string[] strs = { @"{""a"" : ""a1"", ""b"" : ""b1""}", @"{""c"" : ""c1"", ""d"" : ""d1""}" };
+            if (strs.Length != 0)//from args
+            {
+                foreach (string ss in strs)
+                {
+                    Console.WriteLine(ss);
+                    redisCli.LPush(list, Encoding.Default.GetBytes(ss));//push into the head of list
+                }
+            }//if
+
+
+        }
+    }
+
+
+}
